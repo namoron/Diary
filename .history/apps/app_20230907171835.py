4b4669f49e20254672052fe09fb2a30a12f0f677
namor3 <213shirokumals530hl@gmail.com>
2023-09-07 17:19:58 +0900

    p113

---
+++ .history/apps/app_20230907171835.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+#SQLAlchemy  をインスタンス化する
+db = SQLAlchemy()
+csrf = CSRFProtect()
+# create_app 関数を作成する
+def create_app ():
+    # flask インスタンス作成
+    app = Flask(__name__)
+    # アプリ設定をする
+    app.config.from_mapping(
+        SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI="sqlite:///" +
+        str(Path(Path(__file__).parent.parent/ 'local.sqlite')), 
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        #SQLをコンソールログに出力する設定
+        SQLALCHEMY_ECHO=True
+        WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
+    )
+
+    #SQLAlchemyとアプリ連携をする
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    Migrate(app,db)
+    # crud パッケージからviewsをimportする
+    from apps.crud import views as crud_views
+
+    # register_blueprintを使いviewsのcrudをアプリへ登録する
+    app.register_blueprint(crud_views.crud,url_prefix="/crud")
+
+    return app
---
+++ .history/apps/crud/__init___20230907165143.py
@@ -0,0 +1,1 @@
+import apps.crud.modelss
