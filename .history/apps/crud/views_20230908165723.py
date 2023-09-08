0f7a4a3f19bcadd9a324bbd2a79f7d1fbfbaa9a8
namor3 <213shirokumals530hl@gmail.com>
2023-09-08 18:22:13 +0900

    p126

---
+++ .history/apps/crud/views_20230908165723.py
@@ -0,0 +1,49 @@
+from flask import Blueprint,render_template,redirect,url_for
+#dbをimportする
+from apps.app import db
+#Userクラスをimportする
+from apps.crud.models import User
+from apps.crud.forms import UserForm
+# Blueprintでcrudアプリを生成する
+crud = Blueprint(
+    "crud",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+)
+
+# indexエンドポイントを作成しindex.htmlを返す
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")  
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "コンソールログを確認してください"
+
+@crud.route("/users/new",methods=["GET","POST"])
+def create_user():
+    #UserForm をインスタンス化
+    form = UserForm()
+    #フォーム値をバリデート
+    if form.validate_on_submit():
+        # ユーザーを作成する
+        user = User(
+            username = form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # ユーザーを追加してコミットする
+        db.session.add(user)
+        db.session.commit()
+        #ユーザーの一覧画面へリダイレクト
+        return redirect(url_for("crud.users"))
+
+    return render_template("crud/create.html",form=form)
+
+@crud.route("/users")
+def users():
+    """ユーザーの一覧を取得する"""
+    users = User.query.all()
+    return render_template("crud/index.html",users=users)
