e6ab4574e900c6fbc6ed4be6c56580033d6828d9
namor3 <213shirokumals530hl@gmail.com>
2023-09-09 21:00:00 +0900

    p194

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,4 +1,7 @@
 from flask import Blueprint,render_template
+from apps.app import db
+from apps.crud.models import User
+from apps.detector.models import UserImage
 
 # template_folderを指定する(static は指定しない
 dt = Blueprint("detector",__name__,template_folder="templates")
@@ -6,4 +9,11 @@ dt = Blueprint("detector",__name__,template_folder="templates")
 #dtアプリケーションを使ってエンドポイントを作成する
 @dt.route("/")
 def index():
-    return render_template("detector/index.html")
+    # UserとUserImageをjoinして画像を取得する
+    user_images = ( 
+        db.session.query(User,UserImage)
+        .join(UserImage)
+        .filter(User.id == UserImage.user_id)
+        .all()
+    )
+    return render_template("detector/index.html",user_images=user_images)
